77d98f8147727fcc567b4e020c850774ffd6a5dc
ktsourdi <ktsourdinis@sidroco.com>
2024-08-09 14:00:28 +0300

    fix: styling on selected category, dropdown

--- src/app/cats/page.tsx
+++ src/app/cats/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React, { useState } from "react";
 import SelectGroupTwo from "@/components/SelectGroup/SelectGroupTwo";
-import CardDataStats from "@/components/CardDataStats";
 import { usePathname } from "next/navigation";
 import "@/css/style.css";
 
 const ECommerce: React.FC = () => {
-  const [selectedOption, setIsOptionSelected] = useState<string>("");
+  const [selectedOption, setSelectedOption] = useState<string>("");
+
   const importAll = (context: any) =>
     context.keys().map((key: string) => context(key).default);
 
@@ -39,68 +39,44 @@ const ECommerce: React.FC = () => {
     ),
   );
 
-  function multiculti(variable: string) {
-    setIsOptionSelected(variable);
-  }
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+  };
 
   return (
     <>
       <div className="grid grid-cols-2 gap-6 bg-center md:grid-cols-2 md:gap-8 xl:grid-cols-2 2xl:gap-10">
         <button
-          onClick={() => multiculti("Male")}
-          className={`card ${selectedOption === "Male" ? "selected" : ""}`}
+          onClick={() => handleOptionChange("cards")}
+          className={`card ${selectedOption === "cards" ? "selected" : ""}`}
         >
-          <CardDataStats
-            title="cards"
-            total="$3.456K"
-            rate={
-              <input
-                checked={selectedOption === "Male"}
-                onChange={() => multiculti("Male")}
-                type="radio"
-                value="Male"
+          <div className="card-title">cards</div>
+          <div>
+            <svg
+              className="fill-primary dark:fill-white"
+              width="22"
+              height="16"
+              viewBox="0 0 22 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 15.1156C4.19376 15.1156 0.825012 8.61876 0.687512 8.34376C0.584387 8.13751 0.584387 7.86251 0.687512 7.65626C0.825012 7.38126 4.19376 0.918762 11 0.918762C17.8063 0.918762 21.175 7.38126 21.3125 7.65626C21.4156 7.86251 21.4156 8.13751 21.3125 8.34376C21.175 8.61876 17.8063 15.1156 11 15.1156ZM2.26876 8.00001C3.02501 9.27189 5.98126 13.5688 11 13.5688C16.0188 13.5688 18.975 9.27189 19.7313 8.00001C18.975 6.72814 16.0188 2.43126 11 2.43126C5.98126 2.43126 3.02501 6.72814 2.26876 8.00001Z"
+                fill=""
               />
-            }
-          >
-            <div>
-              <svg
-                className="fill-primary dark:fill-white"
-                width="22"
-                height="16"
-                viewBox="0 0 22 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11 15.1156C4.19376 15.1156 0.825012 8.61876 0.687512 8.34376C0.584387 8.13751 0.584387 7.86251 0.687512 7.65626C0.825012 7.38126 4.19376 0.918762 11 0.918762C17.8063 0.918762 21.175 7.38126 21.3125 7.65626C21.4156 7.86251 21.4156 8.13751 21.3125 8.34376C21.175 8.61876 17.8063 15.1156 11 15.1156ZM2.26876 8.00001C3.02501 9.27189 5.98126 13.5688 11 13.5688C16.0188 13.5688 18.975 9.27189 19.7313 8.00001C18.975 6.72814 16.0188 2.43126 11 2.43126C5.98126 2.43126 3.02501 6.72814 2.26876 8.00001Z"
-                  fill=""
-                />
-                <path
-                  d="M11 10.9219C9.38438 10.9219 8.07812 9.61562 8.07812 8C8.07812 6.38438 9.38438 5.07812 11 5.07812C12.6156 5.07812 13.9219 6.38438 13.9219 8C13.9219 9.61562 12.6156 10.9219 11 10.9219ZM11 6.625C10.2437 6.625 9.625 7.24375 9.625 8C9.625 8.75625 10.2437 9.375 11 9.375C11.7563 9.375 12.375 8.75625 12.375 8C12.375 7.24375 11.7563 6.625 11 6.625Z"
-                  fill=""
-                />
-              </svg>
-            </div>
-            <div></div>
-          </CardDataStats>
+              <path
+                d="M11 10.9219C9.38438 10.9219 8.07812 9.61562 8.07812 8C8.07812 6.38438 9.38438 5.07812 11 5.07812C12.6156 5.07812 13.9219 6.38438 13.9219 8C13.9219 9.61562 12.6156 10.9219 11 10.9219ZM11 6.625C10.2437 6.625 9.625 7.24375 9.625 8C9.625 8.75625 10.2437 9.375 11 9.375C11.7563 9.375 12.375 8.75625 12.375 8C12.375 7.24375 11.7563 6.625 11 6.625Z"
+                fill=""
+              />
+            </svg>
+          </div>
         </button>
         <button
-          onClick={() => multiculti("Female")}
-          className={`card ${selectedOption === "Female" ? "selected" : ""}`}
+          onClick={() => handleOptionChange("cats")}
+          className={`card ${selectedOption === "cats" ? "selected" : ""}`}
         >
-          <CardDataStats
-            title="Total Profit"
-            total="$45,2K"
-            rate={
-              <input
-                className="cursor-pointer"
-                checked={selectedOption === "Female"}
-                onChange={() => multiculti("Female")}
-                type="radio"
-                value="Female"
-              />
-            }
-          >
+          <div className="card-title">cats</div>
+          <div>
             <svg
               className="fill-primary dark:fill-white"
               width="20"
@@ -122,17 +98,17 @@ const ECommerce: React.FC = () => {
                 fill=""
               />
             </svg>
-          </CardDataStats>
+          </div>
         </button>
       </div>
       <div>
         <SelectGroupTwo
           photos={
-            selectedOption == "Male" && selectedOption
+            selectedOption === "cards"
               ? [randomPhotos, 1]
               : [cats, 2]
           }
-          imageClass="fixed-size-photo"
+          isLoading={false}
         />
       </div>
     </>
