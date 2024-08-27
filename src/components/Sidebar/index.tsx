41cd1ff89899edbff2353459fb0712774e167987
ktsourdi <ktsourdinis@sidroco.com>
2024-08-27 14:48:11 +0300

    fix: sidebar again without grand category (as it was before)

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { FaFolder } from "react-icons/fa";
 import SidebarItem from "@/components/Sidebar/SidebarItem";
 import ClickOutside from "@/components/ClickOutside";
 import useLocalStorage from "@/hooks/useLocalStorage";
@@ -30,7 +29,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           sidebarOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        {/* <!-- SIDEBAR HEADER --> */}
         <div className="items-left flex justify-between gap-2 px-6 py-5.5 lg:py-6.5">
           <button
             onClick={() => setSidebarOpen(!sidebarOpen)}
@@ -40,34 +38,24 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             {/* Sidebar toggle button SVG */}
           </button>
         </div>
-        {/* <!-- SIDEBAR HEADER --> */}
 
         <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
-          {/* <!-- Sidebar Menu --> */}
           <nav className="mt-5 px-4 py-4 lg:mt-9 lg:px-6">
-            <div>
-              <div className="flex items-center gap-2.5 w-full text-left px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-logolightblue dark:hover:bg-meta-4">
-                <FaFolder />
-                <span>Folders</span>
-              </div>
-              {/* Dropdown content always visible */}
-              <ul className="pl-8">
-                {folders.map((folder, index) => (
-                  <SidebarItem
-                    key={index}
-                    item={{
-                      icon: <FaFolder />,
-                      label: folder.replace(/[_-]/g, " "),
-                      route: `/dashboard/${folder}`,
-                    }}
-                    pageName={pageName}
-                    setPageName={setPageName}
-                  />
-                ))}
-              </ul>
-            </div>
+            <ul className="pl-4">
+              {folders.map((folder, index) => (
+                <SidebarItem
+                  key={index}
+                  item={{
+                    icon: null, // Remove icon if not needed
+                    label: folder.replace(/[_-]/g, " "),
+                    route: `/dashboard/${folder}`,
+                  }}
+                  pageName={pageName}
+                  setPageName={setPageName}
+                />
+              ))}
+            </ul>
           </nav>
-          {/* <!-- Sidebar Menu --> */}
         </div>
       </aside>
     </ClickOutside>
@@ -75,4 +63,3 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 };
 
 export default Sidebar;
-
